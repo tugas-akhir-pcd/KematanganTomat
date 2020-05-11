05fb2a5b4ced6c940c07b6a634ffd5104597c48d
febtrianiari <febtria.niari@gmail.com>
2020-05-11 17:32:41 +0700

    pushbutton1trainingdata

--- project.m
+++ project.m
@@ -1,30 +1,5 @@
 function varargout = project(varargin)
-% PROJECT MATLAB code for project.fig
-%      PROJECT, by itself, creates a new PROJECT or raises the existing
-%      singleton*.
-%
-%      H = PROJECT returns the handle to a new PROJECT or the handle to
-%      the existing singleton*.
-%
-%      PROJECT('CALLBACK',hObject,eventData,handles,...) calls the local
-%      function named CALLBACK in PROJECT.M with the given input arguments.
-%
-%      PROJECT('Property','Value',...) creates a new PROJECT or raises the
-%      existing singleton*.  Starting from the left, property value pairs are
-%      applied to the GUI before project_OpeningFcn gets called.  An
-%      unrecognized property name or invalid value makes property application
-%      stop.  All inputs are passed to project_OpeningFcn via varargin.
-%
-%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
-%      instance to run (singleton)".
-%
-% See also: GUIDE, GUIDATA, GUIHANDLES
 
-% Edit the above text to modify the response to help project
-
-% Last Modified by GUIDE v2.5 08-May-2020 14:51:32
-
-% Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
 gui_State = struct('gui_Name',       mfilename, ...
                    'gui_Singleton',  gui_Singleton, ...
@@ -46,38 +21,110 @@ end
 
 % --- Executes just before project is made visible.
 function project_OpeningFcn(hObject, eventdata, handles, varargin)
-% This function has no output args, see OutputFcn.
-% hObject    handle to figure
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-% varargin   command line arguments to project (see VARARGIN)
-
-% Choose default command line output for project
 handles.output = hObject;
 
-% Update handles structure
 guidata(hObject, handles);
 
-% UIWAIT makes project wait for user response (see UIRESUME)
-% uiwait(handles.figure1);
-
-
-% --- Outputs from this function are returned to the command line.
 function varargout = project_OutputFcn(hObject, eventdata, handles) 
-% varargout  cell array for returning output args (see VARARGOUT);
-% hObject    handle to figure
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
 
-% Get default command line output from handles structure
 varargout{1} = handles.output;
 
 
 % --- Executes on button press in pushbutton1.
 function pushbutton1_Callback(hObject, eventdata, handles)
-% hObject    handle to pushbutton1 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
+
+% pisah rgb
+Img = handles.I;
+%cari nilai HSI
+RGB     = im2double(Img);
+Red     = RGB(:,:,1);
+Green   = RGB(:,:,2);
+Blue    = RGB(:,:,3);
+%Hue
+atas=1/2*((Red-Green)+(Red-Blue));
+bawah=((Red-Green).^2+((Red-Blue).*(Green-Blue))).^0.5;
+teta = acosd(atas./(bawah));
+if Blue >= Green
+    H = 360 - teta;
+else
+    H = teta;
+end
+
+H = H/360;
+[r c] = size(H);
+for i=1 : r
+    for j=1 : c
+        z = H(i,j);
+        z(isnan(z)) = 0; %isnan adalah is not none artinya jika bukan angka dia akan memberi 0
+        H(i,j) = z;
+    end
+end
+%S
+S=1-(3./(sum(RGB,3))).*min(RGB,[],3);
+[r c] = size(S);
+for i=1 : r
+    for j=1 : c
+        z = S(i,j);
+        z(isnan(z)) = 0;
+        S(i,j) = z;
+    end
+end
+%I
+I=(Red+Green+Blue)/3;
+
+MeanR = mean2(Red);
+MeanG = mean2(Green);
+MeanB = mean2(Blue);
+MeanH = mean2(H);
+MeanS = mean2(S);
+MeanI = mean2(I);
+VarRed = var(Red(:)); VarGreen = var(Green(:)); VarBlue = var(Blue(:));
+VarH = var(H(:)); VarS = var(S(:)); VarI = var(I(:));
+RangeR = ((max(max(Red)))-(min(min(Red))));
+RangeG = ((max(max(Green)))-(min(min(Green))));
+RangeB = ((max(max(Blue)))-(min(min(Blue))));
+RangeH = ((max(max(H)))-(min(min(H))));
+RangeS = ((max(max(S)))-(min(min(S))));
+RangeI = ((max(max(I)))-(min(min(I))));
+
+data = get(handles.uitable2,'Data');
+data{1,1} = num2str(MeanR);
+data{2,1} = num2str(MeanG);
+data{3,1} = num2str(MeanB);
+data{4,1} = num2str(MeanH);
+data{5,1} = num2str(MeanS);
+data{6,1} = num2str(MeanI);
+
+data{1,2} = num2str(VarRed);
+data{2,2} = num2str(VarGreen);
+data{3,2} = num2str(VarBlue);
+data{4,2} = num2str(VarH);
+data{5,2} = num2str(VarS);
+data{6,2} = num2str(VarI);
+
+data{1,3} = num2str(RangeR);
+data{2,3} = num2str(RangeG);
+data{3,3} = num2str(RangeB);
+data{4,3} = num2str(RangeH);
+data{5,3} = num2str(RangeS);
+data{6,3} = num2str(RangeI);
+
+set(handles.uitable2,'Data',data,'ForegroundColor',[0 0 0])
+training1 = xlsread('Data Training');
+group = training1(:,25);
+training = [training1(:,1) training1(:,2) training1(:,3) training1(:,4) training1(:,5) training1(:,6) training1(:,7) training1(:,8) training1(:,9) training1(:,10) training1(:,11) training1(:,12) training1(:,13) training1(:,14) training1(:,15) training1(:,16) training1(:,17) training1(:,18)];
+Z=[MeanR MeanG MeanB MeanH MeanS MeanI VarRed VarGreen VarBlue VarH VarS VarI RangeR RangeG RangeB RangeH RangeS RangeI];
+hasil1=knnclassify(Z,training,group);
+
+if hasil1==1
+    x='MATURE';
+elseif hasil1==2
+    x='HALF-MATURE';
+elseif hasil1==3
+    x='IMMATURE';
+end
+set(handles.edit1,'string',x);
+
 
 
 % --- Executes on button press in pushbutton2.
@@ -87,24 +134,12 @@ function pushbutton2_Callback(hObject, eventdata, handles)
 % handles    structure with handles and user data (see GUIDATA)
 
 
+function uitable2_CellEditCallback(hObject, eventdata, handles)
 
 function edit1_Callback(hObject, eventdata, handles)
-% hObject    handle to edit1 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
 
-% Hints: get(hObject,'String') returns contents of edit1 as text
-%        str2double(get(hObject,'String')) returns contents of edit1 as a double
-
-
-% --- Executes during object creation, after setting all properties.
 function edit1_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to edit1 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
 
-% Hint: edit controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
@@ -112,22 +147,9 @@ end
 
 
 function edit2_Callback(hObject, eventdata, handles)
-% hObject    handle to edit2 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
 
-% Hints: get(hObject,'String') returns contents of edit2 as text
-%        str2double(get(hObject,'String')) returns contents of edit2 as a double
-
-
-% --- Executes during object creation, after setting all properties.
 function edit2_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to edit2 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
 
-% Hint: edit controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
